444c3dcd26b7e85c69a272a08728288209c0c803
Irene Abbey <abbeyirene09@gmail.com>
2025-02-22 21:48:25 +0000

    Task 10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, login_view, logout_view, register_view
 
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
-    path('library/<str:name>/', LibraryDetailView.as_view(), name='library_detail')
+    path('library/<str:name>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login', login_view, name='login_view'),
+    path('logout', logout_view, name='logout_view'),
+    path('register', register_view, name='register_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def list_books(request):
@@ -12,3 +14,34 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+
+# Login View
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            login(request, form.get_user)
+            return redirect("home")  # Redirect to homepage after login
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
+
+# Registration View
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            login(request, form.get_user())
+            return redirect("home")
+        else:
+            form = UserCreationForm()
+            return render(request, "registration/register.html", {"form": form})
