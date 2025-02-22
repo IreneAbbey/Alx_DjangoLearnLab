c74a6f23356aa523af333528db211a964cc1fc0f
Irene Abbey <abbeyirene09@gmail.com>
2025-02-22 22:01:33 +0000

    Task 10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import LoginView, LogoutView, list_books, LibraryDetailView, register  # Removed unnecessary views import
+from .views import LoginView, LogoutView, list_books, LibraryDetailView, register  
+from LibraryProject.relationship_app import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<str:name>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login_view'),  # Fixed template_name syntax
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout_view'),  # Fixed template_name syntax
-    path('register/', register, name='register_view'),  # Fixed trailing slash
+    path('login/', LoginView.as_view(template_name='login.html'), name='login_view'),  
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout_view'),  
+    path('register/', views.register, name='register_view'), 
 ]
