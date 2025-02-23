6268245f5ff99073b13460f813690ce19eb52ee0
Irene Abbey <abbeyirene09@gmail.com>
2025-02-23 15:41:58 +0000

    Task10

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,7 +13,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     class Meta:
-        Permissions = [
+        permissions = [
             ("can_add_book", "Can add book"),
             ("can_change_book", "Can edit book"),
             ("can_delete_book", "Can delete book")
