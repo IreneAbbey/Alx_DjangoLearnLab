bb25c1b0a82e2044337b25385665728454c69f3e
Irene Abbey <abbeyirene09@gmail.com>
2025-03-06 23:21:44 +0000

    task 12

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    release_date = models.DateField()
-    genre = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+
 
     def __str__(self):
         return self.title
