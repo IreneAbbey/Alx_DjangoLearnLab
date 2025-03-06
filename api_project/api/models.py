ab1236464dc25206f29be606a4c4f81c23a53b52
Irene Abbey <abbeyirene09@gmail.com>
2025-03-06 23:19:38 +0000

    task 12

---
+++ api_project/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
