d8fb5d9643e3112329856bb8bd914808882109b0
Irene Abbey <abbeyirene09@gmail.com>
2025-03-06 23:38:36 +0000

    task 12

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,8 @@
 import rest_framework
+import rest_framework.serializers
 
 
-class BookSerializer(rest_framework.serializer.ModelSerializer):
+class BookSerializer(rest_framework.serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'title', 'author', 'publication_date']
