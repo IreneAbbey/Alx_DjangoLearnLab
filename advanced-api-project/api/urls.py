7ad3bc6ea6b4e6900ae790f961f9a3c27c15d220
Irene Abbey <abbeyirene09@gmail.com>
2025-03-11 21:38:15 +0000

    Task 13

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('books/', ListView.as_view()),
     path('books/<int:pk>/', DetailView.as_view()),
     path('books/create/', CreateView.as_view()),
-    path('books/<int:pk>/update/', UpdateView.as_view()),
-    path('books/<int:pk>/delete/', DeleteView.as_view()),
+    path('books/update/<int:pk>/', UpdateView.as_view()),
+    path('books/delete/<int:pk>/', DeleteView.as_view()),
 ]
