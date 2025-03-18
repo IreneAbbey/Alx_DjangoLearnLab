eaea1e373c876a7aa4a725cdc053be868c69ca07
Irene Abbey <abbeyirene09@gmail.com>
2025-03-18 11:16:37 +0000

    feat: Added blog app

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +68,7 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 
@@ -76,10 +77,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'mystrongpassword',
+        'HOST': 'localhost',  
+        'PORT': '8000',  
     }
 }
+
 
 
 # Password validation
