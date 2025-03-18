27f89938daeca6c76e05ffa189cf738e3e774640
Irene Abbey <abbeyirene09@gmail.com>
2025-03-18 13:37:30 +0000

    feat: Added views

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k$=y2x=%4#0tt4gz&*x=0qh19dq2+&@kao&0p7p7@ds7a7n)vw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -126,7 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
