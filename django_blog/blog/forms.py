84b56c4cf5f0b378b6e14b4332072a43b6e5db51
Irene Abbey <abbeyirene09@gmail.com>
2025-03-21 12:32:57 +0000

    Fix tagging and search functionality to the blog project

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,11 +1,15 @@
 from .models import Post, Comment
 from django import forms
+from taggit.forms import TagWidget
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tag']
+        widgets ={
+            'tags' : TagWidget()
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
