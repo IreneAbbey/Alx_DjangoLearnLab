156c01109ff7a87bbba5edb18d42b252c3868d54
Irene Abbey <abbeyirene09@gmail.com>
2025-03-21 11:37:48 +0000

    Implemented full blog CRUD features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
