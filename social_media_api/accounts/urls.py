6d0f0cd322d79fff58f4655c466e94f800c3109b
Irene Abbey <abbeyirene09@gmail.com>
2025-03-25 10:54:37 +0000

    feat: URL Configurations

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, login_view, ProfileView
+from .views import RegisterView, login_view, ProfileView, FollowUserView, UnfollowUserView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', login_view, name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user')
 ]
