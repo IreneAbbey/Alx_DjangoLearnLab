aa77c8de4ed805019ee6155c3b52f86fac1cbdb0
Irene Abbey <abbeyirene09@gmail.com>
2025-03-25 11:38:45 +0000

    fix: Fixed like view

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework import viewsets, permissions
 from.models import Like, Post, Comment
 from.serializers import PostSerializer, CommentSerializer
@@ -46,8 +46,8 @@ class UserFeedView(generics.GenericAPIView):
 class LikePostView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, post_id):
-        post = Post.objects.get(post_id=post_id)
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user= request.user, post = post)
         if created:
             Notification.objects.create(recipients=post.author, actor = request.user, verb="liked your post",  target = post)
